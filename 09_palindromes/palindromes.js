37c69a03bcd3eb2f96e6de8b9a1d28d986f503a9
Mishiez <michellegathonimain@gmail.com>
2024-08-29 18:07:08 +0300

    Managed to create a function that  reverses a string while taking out all white spaces and punctuations

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let punctuationless=str.match(/\w/g).join("");
 
-};
+    let lowerCasePunctuationless=punctuationless.toLowerCase();
+    let reversePunctuationless=lowerCasePunctuationless.split('').reverse().join('');
+    
+    return reversePunctuationless===lowerCasePunctuationless;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
