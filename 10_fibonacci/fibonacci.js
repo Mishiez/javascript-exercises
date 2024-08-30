34bd9abeca4c4457b7f22b42dc454feb2c48169c
Mishiez <michellegathonimain@gmail.com>
2024-08-30 18:50:00 +0300

    Create a series of number in which the last number in the sequence is the sum of the two preceding numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num==0){
+        return 0;
+    }
+    else if(num<0){
+        return "OOPS";
+    }
+    else{
+        let sequence=[1,1];
+        for(let i=2;i<=num;i++){
+            sequence[i]=sequence[i-2]+sequence[i-1];
+        }
+        return sequence[num-1];
+    }
 
 };
 
