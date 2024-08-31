84824118c8ceddc7b3c27d15ce5534e5c15e6257
Mishiez <michellegathonimain@gmail.com>
2024-08-31 18:26:03 +0300

    Finished the last exercie of the repo by finding t he oldest person from an array of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
+
+    return people.reduce((res, person) => {
+      const age = (person.yearOfDeath || thisYear) - person.yearOfBirth;
+      return age > res.age ? { person, age } : res;
+    }, { person: null, age: 0 }).person;
+  
 
 };
 
